5e3129068b2a76591ce6572679ee2ada79d387f7
pedrete3000 <pedrete3000@hotmail.com>
2022-06-28 16:04:16 +0200

    ejercicio 3 hecho por mi

---
+++ FDLP/src/ejercicio3/Ejercicio3.java
@@ -0,0 +1,24 @@
+package ejercicio3;
+
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int nota1, nota2, nota3;
+		
+		System.out.println("introduzca la primera nota : ");
+		nota1 = sc.nextInt();
+		
+		
+		System.out.println("introduzca la segunda nota : ");
+		nota2 = sc.nextInt();
+		
+		System.out.println("introduzca la tercera nota : ");
+		nota3 = sc.nextInt();
+
+		System.out.println("La media de sus notas es: "+(((float)nota1+nota2+nota3)/3));
+	}
+
+}
