c7e45b2eb82da3561575d812a5ec9cd7a42ab4e9
pedrete3000 <pedrete3000@hotmail.com>
2022-07-03 07:44:09 +0200

    M5 ejercicio 3 hecho en casa

--- M5Ejercicio3/src/m5ejercicio3/M5ejercicio3.java
+++ M5Ejercicio3/src/m5ejercicio3/M5ejercicio3.java
@@ -1,9 +1,24 @@
 package m5ejercicio3;
 
+import java.util.Scanner;
+
 public class M5ejercicio3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Scanner sc = new Scanner(System.in);
+		byte aleatorio = (byte) Math.ceil((Math.random() * 10));
+		int numero;
+		
+		do {
+		System.out.println("Introduce un número entre 0 y 10 : ");
+		numero = sc.nextByte();
+		sc.nextLine();
+		}
+		while (numero != aleatorio);
+		
+		System.out.println("Enhorabuena, has acertado :) el número era "+numero);
+		
 
 	}
 
