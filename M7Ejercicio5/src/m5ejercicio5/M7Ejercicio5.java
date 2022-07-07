739e54817a9c8ead7634948c7ac2c7c295347d5e
pedrete3000 <pedrete3000@hotmail.com>
2022-07-07 09:17:27 +0200

    mas ajustes

--- M7Ejercicio5/src/m5ejercicio5/M7Ejercicio5.java
+++ M7Ejercicio5/src/m5ejercicio5/M7Ejercicio5.java
@@ -4,12 +4,21 @@ public class M7Ejercicio5 {
 
 	public static void main(String[] args) {
 		
-		int a=100, i;
+		/*int a=100, i;
 		
 		for (i=0; i<51;i++) {
 			System.out.println(a);
 			a = a-2;
+		mi forma de hacerlo*/
+		
+		/* forma de hacerlo del profe */
+		int i;
+		
+	for (i=100; i >=0; i--) {
+		if (i%2 == 0) {
+			System.out.println(i);
 		}
+	}
 
 	}
 
