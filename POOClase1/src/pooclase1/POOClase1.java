aca7513c817b6422250fe7251bccd17b3596f6f2
pedrete3000 <pedrete3000@hotmail.com>
2022-07-14 13:22:09 +0200

    mas ajustes sinergics 14 julio

---
+++ POOClase1/src/pooclase1/POOClase1.java
@@ -0,0 +1,24 @@
+package pooclase1;
+
+import java.util.Scanner;
+
+public class POOClase1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		Coche car1= new Coche("Ford", "mondeo", 60, 200, "negro");
+		
+		
+		
+		
+		if (car1.equals.car2) {░
+			System.out.println("Son iguales");
+		} else {
+			System.out.println("Son diferentes");
+		}
+
+	}
+
+}
