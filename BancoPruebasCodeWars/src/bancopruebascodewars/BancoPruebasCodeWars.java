907895e827c76018abea295ecb2b375a5b40e29d
pedrete3000 <pedrete3000@hotmail.com>
2022-08-31 18:51:24 +0200

    ejercicios 31 agosto

--- BancoPruebasCodeWars/src/bancopruebascodewars/BancoPruebasCodeWars.java
+++ BancoPruebasCodeWars/src/bancopruebascodewars/BancoPruebasCodeWars.java
@@ -6,9 +6,8 @@ public class BancoPruebasCodeWars {
 		
 		int [] arr=  {3,2,3,6,4,1,2,3,2,1,2,2,2,1};
 		
-		System.out.println(Acum.getPeaks(arr).toString());
+		System.out.println(Otraspruebas.race(720, 850, 70));
 		
-		System.out.println("pos: "+Acum.getPeaks(arr).keySet()+" peaks: "+Acum.getPeaks(arr).values());
 		
 	}
 			
--- BancoPruebasCodeWars/src/bancopruebascodewars/Otraspruebas.java
+++ BancoPruebasCodeWars/src/bancopruebascodewars/Otraspruebas.java
@@ -1,61 +1,20 @@
 package bancopruebascodewars;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 
 public class Otraspruebas {
 
-	public static String encrypt(final String text, int n) {
+public static int[] race(int v1, int v2, int g) {
+    int totalSecondsTaken = 0;
+    if (v2 > v1 ) {
+      totalSecondsTaken = (g*3600) / (v2-v1) ;
+      int minutos = totalSecondsTaken % 3600;
+      int segundos = totalSecondsTaken % 60;
+      System.out.println(minutos);
+      System.out.println(segundos);
+    } else {
+      return null;
+    }
+    return new int[] {totalSecondsTaken/3600, (totalSecondsTaken % 3600)/60, totalSecondsTaken % 60};
 
-		String todo;
-
-		if (text == null || n < 1) {
-			return text;
-
-		} else {
-
-			ArrayList<Character> odd = new ArrayList<Character>();
-			ArrayList<Character> even = new ArrayList<Character>();
-			String number = text;
-			String total = "";
-
-			for (int i = 0; i < number.length(); i++) {
-
-				if (i % 2 == 0) {
-					even.add(number.charAt(i));
-				} else {
-					odd.add(number.charAt(i));
-				}
-			}
-
-			for (char a : odd) {
-				total += a;
-			}
-			for (char a : even) {
-				total += a;
-			}
-			todo = total;
-		}
-
-		return encrypt(todo, --n);
-	}
-
-	public static String decrypt(final String encryptedText, int n) {
-
-
-            if (n <= 0 || encryptedText == null || encryptedText.isEmpty()) {
-                    return encryptedText;
-            }
-
-            StringBuilder text = new StringBuilder();
-            final int half = encryptedText.length() / 2;
-            for (int i = 0; i < half; i++) {
-                    text.append(encryptedText.charAt(half + i)).append(encryptedText.charAt(i));
-            }
-            if (encryptedText.length() % 2 == 1) {
-                    text.append(encryptedText.charAt(encryptedText.length() - 1));
-            }
-
-            return decrypt(text.toString(), --n);
-	}
+  }
 }
