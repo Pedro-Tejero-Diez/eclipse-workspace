222ed6cf356543d2d8fa1467c51dcc7034244728
pedrete3000 <pedrete3000@hotmail.com>
2022-09-01 09:02:16 +0200

    Ejercicios matinales 1SEPT CASA

--- BancoPruebasCodeWars/src/bancopruebascodewars/Acum.java
+++ BancoPruebasCodeWars/src/bancopruebascodewars/Acum.java
@@ -1,36 +1,27 @@
 package bancopruebascodewars;
 
-import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.List;
+
 public class Acum {
-
 		    
-		    public static Map<String, List<Integer>> getPeaks(int[] arr) {
-				String pos = "";
-				ArrayList<Integer> peaks = new ArrayList<Integer>();
-				Map<String, List<Integer>> map = new HashMap<String, List<Integer>>();
-				for (int i = 1; i < arr.length; i++) {
-					if (arr[i] > arr[i - 1]) {
-						if (arr[i] > arr[i + 1]) {
-							peaks.add(arr[i]);
-							pos += i;
-						} else if (arr[i] == arr[i + 1]) {
-							for (int j = i; j < arr.length; j++) {
-								if (arr[j] < arr[i]) {
-									peaks.add(arr[i]);
-									pos += i;
-								}
-							}
-						}
-					}
-
-				}
-
-				map.put(pos, peaks);
-
-				return map;
-		    }
+	  public static String high(String s) {
+		  HashMap<String, Integer> valuesMap = new HashMap<String, Integer>();
+		  for (int i=97; i<123; i++ ) {
+		  	valuesMap.add()
+		  }
+	  }
+		 
+		  char[] acum;
+		  
+		  for (int i=0; i<s.length();i++) {
+			  int j=0;
+			  while (j<s.length() && s.charAt(i)!=32) {
+				  acum[j] = s.charAt(i);
+			  }
+			  j++;
+		  }
+		  
+		  
+		  
+		    return "";
 		}
